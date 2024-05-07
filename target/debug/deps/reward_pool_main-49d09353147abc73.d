47e6d64f01abfe2a0926699e1bcb0de7cbeea5d4
scab24 <git.seco@protonmail.com>
2024-05-07 10:21:46 +0200

    staking

--- target/debug/deps/reward_pool_main-49d09353147abc73.d
+++ target/debug/deps/reward_pool_main-49d09353147abc73.d
@@ -1,5 +1,5 @@
-/Users/secoalba/Documents/PROGRAMACION/BLOCKCHAIN_DEV/ZEALYNX/DEV/Solana/Staking/target/debug/deps/reward_pool_main-49d09353147abc73: programs/reward_pool_main/src/lib.rs
+/Users/secoalba/Documents/PROGRAMACION/BLOCKCHAIN_DEV/ZEALYNX/DEV/Solana/staking_reward_pool/target/debug/deps/reward_pool_main-49d09353147abc73: programs/reward_pool_main/src/lib.rs
 
-/Users/secoalba/Documents/PROGRAMACION/BLOCKCHAIN_DEV/ZEALYNX/DEV/Solana/Staking/target/debug/deps/reward_pool_main-49d09353147abc73.d: programs/reward_pool_main/src/lib.rs
+/Users/secoalba/Documents/PROGRAMACION/BLOCKCHAIN_DEV/ZEALYNX/DEV/Solana/staking_reward_pool/target/debug/deps/reward_pool_main-49d09353147abc73.d: programs/reward_pool_main/src/lib.rs
 
 programs/reward_pool_main/src/lib.rs:
